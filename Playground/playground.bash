e09d3b112712ffcc72ac65472091b9bca3c4cdbe
ofekzil <ofekzil162@gmail.com>
2024-01-14 15:27:49 -0800

    added more CLI commands, and configured lambda and SNS to woek with processing rekognition results

--- Playground/playground.bash
+++ Playground/playground.bash
@@ -4,7 +4,12 @@
 # Start celebrity recognition for 66 second Big Lebowski clip
 # returns a JobID for getting the values
 # TODO: try out with SNS Topic 
+# prev in us-west-1
 aws rekognition start-celebrity-recognition --video "S3Object={Bucket=testrecognitionvideos,Name=videos/LebowskiLenin.mp4}"
+
+# current in us-west-2
+aws rekognition start-celebrity-recognition --video "S3Object={Bucket=recognitionvideos,Name=TestVideos/LebowskiLenin.mp4}" 
+    \ --notification-channel SNSTopicArn="arn:aws:sns:us-west-2:759623241699:AmazonRekognitionTestTopic",RoleArn="arn:aws:iam::759623241699:role/rekognition-rol-test"
 
 # get unfiltered recognition
 aws rekognition get-celebrity-recognition --job-id 0054abfcc01168b3f59f431791c7c63ef1088771671085c1b9348278ef090e28
