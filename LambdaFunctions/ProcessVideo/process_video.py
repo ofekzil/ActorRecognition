1b8d20d5c45817251e6e085b335ee6241379f47a
ofekzil <ofekzil162@gmail.com>
2024-01-18 20:56:37 -0800

    added lambda function to call start_celebrity_recognition when a new video is uploaded to S3 bucket

---
+++ LambdaFunctions/ProcessVideo/process_video.py
@@ -0,0 +1,35 @@
+# code for lambda to process the result of a start-celebrity-recognition call
+# the lambda is subscribed to an SNS topic that rekognition sends the result to
+
+# before invoking this lambda, we need to do configuration for SNS subscription for lambda, permissions for using rekognition
+# which involves creating different roles, 
+
+# ideally this processing lambda will also calculate the windows/durartions each actor will appear on screen at a time
+# (using a custom algorithm), then store the results in some database
+
+# this code is modelled on SNS-Lambda configuration tutorial in AWS docs
+import boto3
+import json
+
+rek_client = boto3.client('rekognition')
+
+def lambda_handler(event, context):
+    print("Started lambda function")
+    for record in event['Records']:
+        process_message(record)
+    print("done")
+
+def process_message(record):
+    try:
+        message = record['Sns']['Message']
+        print(f"Processed message {message}")
+        message_json = json.loads(message)
+        jobId = message_json['JobId']
+        print(jobId)
+        res = rek_client.get_celebrity_recognition(JobId=jobId)
+        res_str = json.dumps(res)
+        print(res_str)
+        
+    except Exception as e:
+        print("An error occurred")
+        raise e
---
+++ LambdaFunctions/VideoUpload/video_uploaded.py
@@ -0,0 +1,53 @@
+# lambda to function to trigger asynchronous call to Rekognition to process a newly uploaded video
+# the trigger for this lambda is whener a new item is added to the S3 bucket
+# will call start_celebrity_recognition and pass information for SNS Topic to write results to
+
+# modelled on AWS tutorial for using S3 to trigger lambda
+
+import json
+import urllib.parse
+import boto3
+import os
+
+print('Loading function')
+
+SNS_TOPIC_ARN = os.environ.get('SNS_TOPIC_ARN')
+ROLE_ARN = os.environ.get('ROLE_ARN')
+
+s3 = boto3.client('s3')
+rekognition = boto3.client('rekognition')
+
+
+def lambda_handler(event, context):
+    print("Received event: " + json.dumps(event, indent=2))
+
+    # bucket can be used for processing with rekognition
+    bucket = event['Records'][0]['s3']['bucket']['name']
+    print("Bucket = " + bucket)
+    # key is the video name - can be used directly in rekognition
+    key = urllib.parse.unquote_plus(event['Records'][0]['s3']['object']['key'], encoding='utf-8')
+    print("Key = " + key)
+    s3_vid = {
+            'S3Object': {
+                'Bucket' : bucket,
+                'Name' : key
+            }
+        }
+    sns_channel = {
+        'SNSTopicArn' : SNS_TOPIC_ARN,
+        'RoleArn' : ROLE_ARN
+    }
+    try:
+        response = rekognition.start_celebrity_recognition(Video=s3_vid, NotificationChannel=sns_channel)
+        print("Receievd response from start_celebrity_recognition: ")
+        print(json.dumps(response))
+        # response = s3.get_object(Bucket=bucket, Key=key)
+        # print("Received response from S3 get_object: ")
+        # print(json.dumps(response, indent=2, sort_keys=True, default=str))
+        # print("CONTENT TYPE: " + response['ContentType'])
+        # return response['ContentType']
+    except Exception as e:
+        print(e)
+        print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+        raise e
+              
